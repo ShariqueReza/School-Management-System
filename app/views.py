48ce4d8fed246573085e7d3883f7a585ebc1fa7e
Sharique <shariquereza786@gmail.com>
2025-01-06 16:44:38 +0530

    added teacher url

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from app.models import teachers
 from django.http import HttpResponse
+
+
 # Create your views here.
 def index_page(request):
     teacher=teachers.objects.all()
     context={'teacher':teacher}
     return render(request,'app/index.html',context)
+
+def teacher_page(request):
+    teacher=teachers.objects.all()
+    context={'teacher':teacher}
+    return render(request,'app/teachers.html',context)
