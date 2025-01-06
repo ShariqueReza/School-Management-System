763110b6bd1b9d4b0bc8b6ebfbbb9b3670af90ac
Sharique <shariquereza786@gmail.com>
2025-01-06 11:58:41 +0530

    made teachers model & did migrations

--- app/models.py
+++ app/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 class teachers(models.Model):
     name=models.CharField(max_length=100)
-    phone_number=models.IntegerField(max_length=20)
-    slug=models.SlugField(max_length=200,unique=True)
+    Email=models.EmailField(max_length=200,null=True)
+    phone_number=models.IntegerField
+    subjects=models.CharField(max_length=300,null=True)
+    class_teahcer_of=models.IntegerField
+    head_of=models.CharField(max_length=200,null=True)
+
+    
