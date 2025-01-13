a5dc75d38622c7ac05dec0053879cd8c60452213
Sharique <shariquereza786@gmail.com>
2025-01-13 14:41:17 +0530

    migration done for feedback page

--- app/models.py
+++ app/models.py
@@ -9,4 +9,10 @@ class teachers(models.Model):
     class_teacher_of=models.IntegerField(null=True, blank=True)
     position=models.CharField(max_length=200,null=True, blank=True)
 
+class feedback(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100,null=True)
+    comment=models.CharField(max_length=500)
+
+
     
