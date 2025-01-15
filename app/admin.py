253860c91eb1efce1be385bc0cc3b08f5ba2b9f4
Sharique <shariquereza786@gmail.com>
2025-01-15 12:59:04 +0530

    added feedback model in admin.py so that we can edit

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import teachers
+from app.models import teachers,feedback
 
 # Register your models here.
 admin.site.register(teachers)
+admin.site.register(feedback)
