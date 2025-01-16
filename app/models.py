1c2185b6c8873f7b06d9c4321ddd7ab312dc1a22
Sharique <shariquereza786@gmail.com>
2025-01-16 12:51:33 +0530

    added a slug field in Student model and migrated

--- app/models.py
+++ app/models.py
@@ -18,9 +18,10 @@ class Student(models.Model):
     st_name=models.CharField(max_length=100)
     address=models.CharField(max_length=400)
     contact_no=models.IntegerField(null=True)
-    parents_names=models.CharField(max_length=200)
+    parents_names=models.CharField(max_length=400)
     blood_gr=models.CharField(max_length=10)
     emergency=models.IntegerField(null=True)
+    slug=models.SlugField(max_length=200,unique=True,blank=True)
 
 
 
