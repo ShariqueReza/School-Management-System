836c8735c0daa21e517505b585a6c09ad3c86699
Sharique <shariquereza786@gmail.com>
2025-01-16 11:41:01 +0530

    registered student model in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from app.models import teachers,feedback
+from app.models import teachers,feedback,Student
 
 # Register your models here.
 admin.site.register(teachers)
 admin.site.register(feedback)
+admin.site.register(Student)
