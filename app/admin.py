370a93414ca36b5969f052061adb494a925809fe
Sharique <shariquereza786@gmail.com>
2025-01-17 12:27:53 +0530

    fixed some errors in models

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app.models import teachers,feedback,Student
+from app.models import teachers,feedback,all_students,Student
 
 # Register your models here.
 admin.site.register(teachers)
 admin.site.register(feedback)
+admin.site.register(all_students)
 admin.site.register(Student)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class teachers(models.Model):
@@ -9,6 +10,7 @@ class teachers(models.Model):
     class_teacher_of=models.IntegerField(null=True, blank=True)
     position=models.CharField(max_length=200,null=True, blank=True)
 
+
 class feedback(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=100,null=True)
@@ -16,19 +18,26 @@ class feedback(models.Model):
 
 class all_students(models.Model):
     class_name = models.CharField(max_length=50,blank=True)
+    slug=models.SlugField(max_length=200,unique=True,null=True)
+
+    def save(self,*args, **kwargs):
+        if not self.id:
+            self.slug=slugify(self.class_name)
+        return super(all_students,self).save(*args, **kwargs)
+    
     def __str__(self):
         return self.class_name
 
 
 class Student(models.Model):
-    class_name = models.ForeignKey(all_students, on_delete=models.CASCADE, related_name='students',null=True)
+    class_name = models.ForeignKey(all_students, on_delete=models.CASCADE, related_name='students',null=True,blank=True)
     st_name=models.CharField(max_length=100)
     address=models.CharField(max_length=400)
     contact_no=models.IntegerField(null=True)
     parents_names=models.CharField(max_length=400)
     blood_gr=models.CharField(max_length=10)
     emergency=models.IntegerField(null=True)
-    slug=models.SlugField(max_length=200,unique=True,blank=True)
+
 
 
 
