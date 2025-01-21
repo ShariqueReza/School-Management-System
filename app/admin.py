0669475509933dc9d86c404344d8d7c42b66362e
Sharique <shariquereza786@gmail.com>
2025-01-21 15:50:00 +0530

    added all_exams & Exam in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.models import teachers,feedback,all_students,Student,all_results,Result
+from app.models import teachers,feedback,all_students,Student,all_results,Result,all_exams,Exam
 
 # Register your models here.
 admin.site.register(teachers)
@@ -8,4 +8,5 @@ admin.site.register(all_students)
 admin.site.register(Student)
 admin.site.register(all_results)
 admin.site.register(Result)
-
+admin.site.register(all_exams)
+admin.site.register(Exam)
