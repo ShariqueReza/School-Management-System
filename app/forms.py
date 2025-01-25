7a60124741106bb5639f0c841f92bce38ff82e48
Sharique <shariquereza786@gmail.com>
2025-01-25 11:56:07 +0530

    added clean_fields for registration page

--- app/forms.py
+++ app/forms.py
@@ -39,3 +39,24 @@ class NewUserForm(UserCreationForm):
         self.fields['email'].widget.attrs['placeholder']='Type your email....'
         self.fields['password1'].widget.attrs['placeholder']='Type your password....'
         self.fields['password2'].widget.attrs['placeholder']='Repeat your password....'
+    
+    def clean_username(self):
+        username=self.cleaned_data['username'].lower()
+        new=User.objects.filter(username=username)
+        if new.count():
+            raise forms.ValidationError("user already exist")
+        return username
+    
+    def clean_email(self):
+        email=self.cleaned_data['email'].lower()
+        new=User.objects.filter(email=email)
+        if new.count():
+            raise forms.ValidationError("Email already exist")
+        return email
+    
+    def clean_password2(self):
+        password1=self.cleaned_data['password1']
+        password2=self.cleaned_data['password2']
+        if password1 and password2 and password1!=password2:
+            raise forms.ValidationError("password don't match")
+        return password2
